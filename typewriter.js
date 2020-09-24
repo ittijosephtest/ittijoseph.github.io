9fe3d94f069c6999287777d55bc5d5d5ee0a1f38
Itti Joseph <58567407+IttiJoseph@users.noreply.github.com>
2020-09-24 21:53:00 +0530

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -5,7 +5,7 @@ var retype = {
 	//		  It works cleaner if neighboring words have different first letters.
 	//		  Spaces in phrases can cause a hiccup. Best practice to keep phrases as single words.
 	retypePhrases: [
-		'Jazz pianist',
+		'Jazz pianist.',
         'Analog synth nerd.',
 		'Product designer.'
 	],
